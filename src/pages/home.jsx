6fb9c12755e70a09583d0d3937001b6bec84baa2
junaid <jk7948677@gmail.com>
2026-09-23 17:41:04 +0530

    Added added home post header

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -42,6 +42,7 @@ function Home({ logged }) {
   return (
     <div className="post-container">
         {!logged && <h1>Sign up & login to see them posts </h1>}
+        {logged && <h1>All the posts.</h1>}
         { posts.map( post => (
             <div key={post.id} className="post-div" >
                 <Link to={`/posts/${post.id}`}>
